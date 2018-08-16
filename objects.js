08702a9d23bcc83c2cdbb2c28a198eddfa8b22b9
samantha <sstephens614@yahoo.com>
2018-08-16 02:42:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,13 @@ function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName] =
 function removeFromPlaylist(playlist, artistName) {
   // console.log(playlist.keys)
 
+<<<<<<< HEAD
   delete playlist[artistName];
   
   return playlist
+=======
+  delete playlist.artistName;
+  
+  // return playlist
+>>>>>>> 9416ab5203d17b43e4eb8f8d3a95a95b0c3b1443
 }
